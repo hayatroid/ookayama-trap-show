db3c5cdef3cb7ec6947f8b3b75d08eed3fbd9a56
Hayato <114300461+hayatroid@users.noreply.github.com>
2025-10-27 09:39:13 +0900

    つくばへのアクセス (#4)

--- frontend/src/components/PageMain.tsx
+++ frontend/src/components/PageMain.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import { BrowserRouter, Link, Routes, Route } from "react-router";
-import { AiFillHome } from "react-icons/ai";
+import { FaHome, FaMapMarkerAlt } from "react-icons/fa";
 
 import TopPage from "./TopPage";
 import UserPage from "./UserPage";
@@ -42,13 +42,50 @@ const Header = styled.header`
   }
 `;
 
+const Footer = styled.footer`
+  color: #999;
+  font-size: 14px;
+  margin: 24px 0 0 0;
+  padding-top: 8px;
+  border-top: solid 1px #eee;
+  display: flex;
+  justify-content: flex-end;
+  flex-wrap: wrap;
+  gap: 8px;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+    text-underline-offset: 4px;
+    cursor: pointer;
+    display: block;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
 const Left = styled.div`
   display: flex;
   gap: 16px;
-  align-item: center;
+  align-items: center;
+`;
+
+const Right = styled.div`
+  display: flex;
+  gap: 16px;
+  align-items: center;
+}
 `;
 
 const HomeLink = styled(Link)`
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+`;
+
+const MapLink = styled.a`
   font-size: 16px;
   display: flex;
   align-items: center;
@@ -72,9 +109,9 @@ const PageMain = ({ wrapperRef, ref }: PageMainProps) => {
         <Header>
           <Left>
             <HomeLink to="/">
-              <AiFillHome />
+              <FaHome />
             </HomeLink>
-            <a onClick={scrollToTop}>上スクロールで登録／サインイン</a>
+            <a onClick={scrollToTop}>上スクロールで登録 ／ サインイン</a>
           </Left>
           <a href="https://github.com/hayatroid/ookayama-trap-show">
             GitHub ／ 使い方
@@ -84,6 +121,14 @@ const PageMain = ({ wrapperRef, ref }: PageMainProps) => {
           <Route path="/" element={<TopPage />} />
           <Route path="/:screenName" element={<UserPage />} />
         </Routes>
+        <Footer>
+          <Right>
+            <MapLink href="https://www.google.com/maps/dir/?api=1&origin=titech+museum&destination=tsukuba+amanogawa">
+              <FaMapMarkerAlt />
+            </MapLink>
+            <a href="https://tsukuba.yokohama.dev">筑波大学 80km →</a>
+          </Right>
+        </Footer>
       </Wrapper>
     </BrowserRouter>
   );
